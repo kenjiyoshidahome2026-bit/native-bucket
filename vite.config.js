dc8931a296659ae7175da1e7733f15c25fb2cf1e
Kenji <kenji.yoshida.home.2026@gmail.com>
2026-04-03 10:12:03 +0900

    yml

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ const banner = `/*!
 
 export default defineConfig({
   // エラーの原因となっていた plugins: [cloudflare()] を削除しました
-  base: './',
+  base: '/native-bucket/',
   build: {
     sourcemap: true,
     minify: 'terser',
