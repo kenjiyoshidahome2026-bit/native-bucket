ca100df4a6fdefcc1c7b7abbc2d5f8413e7d4e82
Kenji Yoshida <kenji.yoshida.home.2026@gmail.com>
2026-04-04 14:14:43 +0900

    class

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function nativeBucket(apiUrl = null) {
 	const bucketyOption = opts => ({ baseUrl: `${API_BASE}/bucket/`, ...opts });
     return {
         Fetch: (url, opt = {}) => _Fetch(url, proxyOption(opt)),
-        Bucket: (dir, opts) => _Bucket(dir, bucketyOption(opts)),
+        Bucket: (dir, opts) => new _Bucket(dir, bucketyOption(opts)),
         Cache
     };
 }
