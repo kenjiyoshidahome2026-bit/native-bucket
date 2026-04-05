8b25b3f6ba97a552c241d1650f8b1cbd125375c0
Kenji Yoshida <kenji.yoshida.home.2026@gmail.com>
2026-04-06 05:18:07 +0900

    offline update

--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ function nativeBucket(apiUrl = null) {
     return {
         Fetch: (url, opt = {}) => _Fetch(url, proxyOption(opt)),
         Bucket: async function(dir, opts) { const instance = new _Bucket(dir, bucketyOption(opts)); 
+            if (instance.offline()) return instance;
       		try { await instance.list(1); return instance;
 			} catch (e) {
                 console.warn(`[native-bucket] Failed to connect to Bucket "${dir}" at ${BUCKET_URL}.`);
-                return instance.offline()? false: null;
+                return null;
             }
 		},
         Cache
