c368da3710d75f8babecf74861b98c32e43bef06
Kenji Yoshida <kenji.yoshida.home.2026@gmail.com>
2026-04-10 18:25:10 +0900

    ALLOWED_DOMAINS

--- workers/cors.js
+++ workers/cors.js
@@ -2,7 +2,7 @@ export function getCorsHeaders(request, env) {
 	const headers = new Headers();
 	const origin = request.headers.get("Origin");
 	if (!origin || origin === "null") return headers;
-	const allowedList = env.ALLOWED_ORIGINS.split(",").map(d => d.trim());
+	const allowedList = env.ALLOWED_DOMAINS.split(",").map(d => d.trim());
 	const isAllowed = allowedList.some(t => !!(origin === t||origin.endsWith("." + t)||origin.endsWith("://" + t)));
 	isAllowed &&headers.set("Access-Control-Allow-Origin", origin);
 	headers.set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, OPTIONS");
